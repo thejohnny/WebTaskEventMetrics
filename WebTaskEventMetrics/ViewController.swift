46d6ea01e1b5097529a3879019cc08c1b4f8170c
John M. Carlin <thejohnny@gmail.com>
2016-07-20 21:50:20 -0500

    First pass on incrementing arbitrary metric via WebTask

--- WebTaskEventMetrics/ViewController.swift
+++ WebTaskEventMetrics/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+
+    let vendorId = UIDevice.currentDevice().identifierForVendor!.UUIDString as String
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            incrementMetric("button1Tapped")
+        case 1:
+            incrementMetric("button2Tapped")
+        default:
+            return
+        }
+    }
 
+    func incrementMetric(metric: String) {
+        let baseURL = NSURL(string: "http://localhost:8080/")
+        let request = NSURLRequest(URL: NSURL(string: "?vendorId=\(vendorId)&metricName=\(metric)", relativeToURL: baseURL)!)
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            if let data = data {
+                let response = NSString(data: data, encoding: NSUTF8StringEncoding)
+                print(response)
+            }
+        }
+        task.resume()
+    }
 }
 
